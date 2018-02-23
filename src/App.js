611e2dc26e0a3c041a109a7025b26b3b7daa9976
Beywer <beywr2@yandex.ru>
2018-02-23 20:53:05 +0400

    Day 4 clean up

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import React, {Component} from 'react';
-import Welcome from "./Welcome.jsx";
-import Clock from "./Clock";
-import List from "./List";
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-                <hr/>
-                <Welcome>, today is a good day to die!</Welcome>
-                <hr/>
-                <Clock/>
-                <List/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Chat</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
