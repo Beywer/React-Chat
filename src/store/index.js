3140f79652da2efa26957cf79079204125dfdc21
Beywer <beywr2@yandex.ru>
2018-03-03 15:14:33 +0400

    Day 4 Redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,33 @@
-import {applyMiddleware, createStore} from 'redux';
+import {applyMiddleware, createStore, compose} from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import logger from 'redux-logger';
 import rootReducer from 'reducers';
 
 export default function configureStore() {
-  const store = createStore(
-    rootReducer,
-    applyMiddleware(thunkMiddleware)
-  );
-  if (process.env.NODE_ENV === 'development') {
-    window.store = store;
+  let store;
+  if (process.env.NODE_ENV === 'production') {
+    store = createStore(
+      rootReducer,
+      applyMiddleware(thunkMiddleware)
+    );
+  } else {
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({serialize: true}) || compose;
+    store = createStore(
+      rootReducer,
+      composeEnhancers(
+        applyMiddleware(thunkMiddleware, logger)
+      )
+    );
   }
+
+  if (process.env.NODE_ENV !== 'production') {
+    window.gstore = store;
+    if (module.hot) {
+      module.hot.accept('reducers', () => {
+        store.replaceReducer(rootReducer);
+      });
+    }
+  }
+
   return store;
 }
